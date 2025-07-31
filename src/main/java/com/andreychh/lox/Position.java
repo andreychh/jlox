12ea22622b88365e3d8aa18bb6f40dbb48bcdaa8
Andrey Chernykh <andreychernykh04@gmail.com>
2025-07-31 15:39:31 +0300

    #1: Add Position, Token, and TokenType classes

---
+++ src/main/java/com/andreychh/lox/Position.java
@@ -0,0 +1,33 @@
+package com.andreychh.lox;
+
+
+public final class Position {
+    private final int line;
+    private final int column;
+
+    public Position(final int line, final int column) {
+        this.line = line;
+        this.column = column;
+    }
+
+    public String format() {
+        return "%d:%d".formatted(this.line, this.column);
+    }
+
+    @Override
+    public String toString() {
+        return this.format();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Position other = (Position) obj;
+        return line == other.line && column == other.column;
+    }
+}
