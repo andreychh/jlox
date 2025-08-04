6fb9ff00ccda81674725070d4e553b7021a4fc9b
Andrey Chernykh <andreychernykh04@gmail.com>
2025-08-04 10:47:00 +0300

    #1: Add parameterized tests for TokenFromLexeme

--- src/test/java/com/andreychh/lox/token/TokenFromLexemeTest.java
+++ src/test/java/com/andreychh/lox/token/TokenFromLexemeTest.java
@@ -2,6 +2,11 @@ package com.andreychh.lox.token;
 
 import com.andreychh.lox.Position;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,17 +14,17 @@ import static org.junit.jupiter.api.Assertions.*;
  * Tests for {@link TokenFromLexeme} class.
  */
 class TokenFromLexemeTest {
-
     /**
-     * TokenFromLexeme formats correctly with inferred token type.
+     * TokenFromLexeme formats correctly with inferred token type for all valid lexemes.
      */
-    @Test
-    void formatIncludesInferredTokenType() {
-        TokenFromLexeme token = new TokenFromLexeme("(", new Position(1, 1));
+    @ParameterizedTest
+    @MethodSource("lexemeToTokenTypeMapping")
+    void formatIncludesInferredTokenType(String lexeme, TokenType expectedType) {
+        TokenFromLexeme token = new TokenFromLexeme(lexeme, new Position(1, 1));
         assertEquals(
-                "Token(LEFT_PAREN, \"(\", 1:1)",
+                String.format("Token(%s, \"%s\", 1:1)", expectedType, lexeme),
                 token.format(),
-                "Token format should include inferred token type"
+                String.format("Token format for '%s' should include inferred token type %s", lexeme, expectedType)
         );
     }
 
@@ -62,6 +67,70 @@ class TokenFromLexemeTest {
                 token1,
                 token2,
                 "Tokens with different positions should not be equal"
+        );
+    }
+
+    /**
+     * TokenFromLexeme throws exception for unexpected lexeme.
+     */
+    @Test
+    void formatThrowsExceptionForUnknownLexeme() {
+        TokenFromLexeme token = new TokenFromLexeme("@unknown$", new Position(1, 1));
+
+        assertThrows(
+                IllegalArgumentException.class,
+                token::format,
+                "Token with unknown lexeme should throw IllegalArgumentException"
+        );
+    }
+
+    /**
+     * Provides test data for token formatting tests.
+     *
+     * @return Stream of arguments containing lexemes and their expected token types
+     */
+    private static Stream<Arguments> lexemeToTokenTypeMapping() {
+        return Stream.of(
+                // Keywords
+                Arguments.of("and", TokenType.AND),
+                Arguments.of("class", TokenType.CLASS),
+                Arguments.of("else", TokenType.ELSE),
+                Arguments.of("false", TokenType.FALSE),
+                Arguments.of("for", TokenType.FOR),
+                Arguments.of("fun", TokenType.FUN),
+                Arguments.of("if", TokenType.IF),
+                Arguments.of("nil", TokenType.NIL),
+                Arguments.of("or", TokenType.OR),
+                Arguments.of("print", TokenType.PRINT),
+                Arguments.of("return", TokenType.RETURN),
+                Arguments.of("super", TokenType.SUPER),
+                Arguments.of("this", TokenType.THIS),
+                Arguments.of("true", TokenType.TRUE),
+                Arguments.of("var", TokenType.VAR),
+                Arguments.of("while", TokenType.WHILE),
+
+                // Single-character operators
+                Arguments.of("{", TokenType.LEFT_BRACE),
+                Arguments.of("}", TokenType.RIGHT_BRACE),
+                Arguments.of("(", TokenType.LEFT_PAREN),
+                Arguments.of(")", TokenType.RIGHT_PAREN),
+                Arguments.of(".", TokenType.DOT),
+                Arguments.of(",", TokenType.COMMA),
+                Arguments.of(";", TokenType.SEMICOLON),
+                Arguments.of("+", TokenType.PLUS),
+                Arguments.of("-", TokenType.MINUS),
+                Arguments.of("*", TokenType.STAR),
+                Arguments.of("/", TokenType.SLASH),
+                Arguments.of("!", TokenType.BANG),
+                Arguments.of("=", TokenType.EQUAL),
+                Arguments.of(">", TokenType.GREATER),
+                Arguments.of("<", TokenType.LESS),
+
+                // Two-character operators
+                Arguments.of("!=", TokenType.BANG_EQUAL),
+                Arguments.of("==", TokenType.EQUAL_EQUAL),
+                Arguments.of(">=", TokenType.GREATER_EQUAL),
+                Arguments.of("<=", TokenType.LESS_EQUAL)
         );
     }
 }
