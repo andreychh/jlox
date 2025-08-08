5e51ec767a3909b7bb661f98ffcdb2fd99a646bd
Andrey Chernykh <andreychernykh04@gmail.com>
2025-08-08 15:45:17 +0300

    #13: Add NumberState for numeric literal lexing and tests

--- src/main/java/com/andreychh/lox/lexing/state/InitialState.java
+++ src/main/java/com/andreychh/lox/lexing/state/InitialState.java
@@ -57,6 +57,7 @@ public final class InitialState implements LexingState {
             case '!', '=', '>', '<' -> new CompoundOperatorState(this.source, this.result);
             case '/' -> new SlashState(this.source, this.result);
             case '"' -> new StringState(this.source, this.result);
+            case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' -> new NumberState(this.source, this.result);
             default -> {
                 Error error = new Error("Unexpected character '%s'.".formatted(character), this.source.position());
                 yield new InitialState(this.source.skip(1), this.result.withError(error));
